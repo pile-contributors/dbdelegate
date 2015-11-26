daca05dc7c02e600741c9305b429fd83abbb401d
Nicu Tofan <nicu.tofan@gmail.com>
2015-11-26 21:14:45 +0200

    removed unused manager; moved to model

--- dbdelegate.cmake
+++ dbdelegate.cmake
@@ -18,10 +18,8 @@ macro    (dbdelegateInit
 
     # compose the list of headers and sources
     set(DBDELEGATE_HEADERS
-        "dbdelegatemanager.h"
         "dbdelegate.h")
     set(DBDELEGATE_SOURCES
-        "dbdelegatemanager.cc"
         "dbdelegate.cc")
     set(DBDELEGATE_QT_MODS
         Core Sql)
