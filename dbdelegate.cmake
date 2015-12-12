946e41fd19ca541070d49d90fc4672a933161cca
Nicu Tofan <nicu.tofan@gmail.com>
2015-12-12 17:33:20 +0200

    Remove verbose debug output

--- dbdelegate.cmake
+++ dbdelegate.cmake
@@ -1,6 +1,6 @@
 
 # enable/disable cmake debug messages related to this pile
-set (DBDELEGATE_DEBUG_MSG ON)
+set (DBDELEGATE_DEBUG_MSG OFF)
 
 # make sure support code is present; no harm
 # in including it twice; the user, however, should have used
